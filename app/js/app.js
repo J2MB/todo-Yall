b66f60838661b31424e5673923a5efb81cd142e4
Ben Simonsen <bsimonse@vt.edu>
2013-10-07 19:48:37 -0400

    There!  NOW it will break again.

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 var j2mb = {};
 
-j2mb.app = angular.module('todoyall',[]);
+j2mb.app = angular.module('todoyall',['ngRoute']);
 
 j2mb.app.config(['$routeProvider', function($routeProvider){
 	$routeProvider.when('/', {templateURL:'partials/todoList.html', controller:TodoController})
