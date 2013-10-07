e4833084e3ddefb53678e4184b4c425c90114290
Ben Simonsen <bsimonse@vt.edu>
2013-10-07 19:46:56 -0400

    Forgot a bracket.

--- app/js/app.js
+++ app/js/app.js
@@ -4,4 +4,4 @@ j2mb.app = angular.module('todoyall',[]);
 
 j2mb.app.config(['$routeProvider', function($routeProvider){
 	$routeProvider.when('/', {templateURL:'partials/todoList.html', controller:TodoController})
-});
+}]);
