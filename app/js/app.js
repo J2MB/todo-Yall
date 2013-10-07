1499a82143a78333c5d06c9db4c51b13ae5df7b9
Ben Simonsen <bsimonse@vt.edu>
2013-10-07 19:51:11 -0400

    take that.

--- app/js/app.js
+++ app/js/app.js
@@ -3,5 +3,5 @@ var j2mb = {};
 j2mb.app = angular.module('todoyall',['ngRoute']);
 
 j2mb.app.config(['$routeProvider', function($routeProvider){
-	$routeProvider.when('/', {templateURL:'partials/todoList.html', controller:TodoController})
+	$routeProvider.when('/awesome', {templateURL:'partials/todoList.html', controller:TodoController})
 }]);
