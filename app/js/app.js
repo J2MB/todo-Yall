c01edcbf3da85674af0b2b707b843be2e80ddf7d
Ben Simonsen <bsimonse@vt.edu>
2013-10-07 19:46:06 -0400

    What's wrong with this picture?

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,7 @@
 var j2mb = {};
 
 j2mb.app = angular.module('todoyall',[]);
+
+j2mb.app.config(['$routeProvider', function($routeProvider){
+	$routeProvider.when('/', {templateURL:'partials/todoList.html', controller:TodoController})
+});
