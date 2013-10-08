7b31e7aeb5c69397795ee7f8273c731331c9ff03
Matt Senn <eternalmatt@gmail.com>
2013-10-07 23:01:49 -0400

    I felt like changing some stuff.

--- app/js/app.js
+++ app/js/app.js
@@ -2,17 +2,25 @@ var j2mb = {};
 
 j2mb.app = angular.module('todoyall',['ngRoute']);
 j2mb.app.config(function($routeProvider) {
-  $routeProvider.
-      when('/', {templateUrl: 'partials/list.html', controller: 'TodoController'}).
-    when('/details/:index', {templateUrl: 'partials/todoItem.html', controller: 'TodoItemController'}).
-      otherwise({redirectTo: '/'});
+  $routeProvider
+      .when('/', {
+          templateUrl: 'partials/list.html', 
+          controller: 'TodoController'
+      })
+      .when('/details/:index', {
+          templateUrl: 'partials/todoItem.html', 
+          controller: 'TodoItemController'
+      })
+      .otherwise({redirectTo: '/'});
 });
 
 j2mb.app.factory("TodoDataService", function(){
     var data = {};
     data.list = [
         { text : "Write this app",
-         done : false }
+          done : false,
+          id: 0
+        }
     ];
     //This is a thing!
     return data;
