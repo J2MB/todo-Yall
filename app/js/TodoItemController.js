4cf5b757f90c5fa4d0d7626ff01d2f684a1377dc
jeffalexander <jeffrey.alan.alexander@gmail.com>
2013-10-07 20:15:32 -0400

    Spiffy

---
+++ app/js/TodoItemController.js
@@ -0,0 +1,4 @@
+j2mb.app.controller("TodoItemController",function($scope, $routeParams){
+    $scope.awesome = "Yaay!";
+    $scope.index = $routeParams.index;
+});
--- app/js/app.js
+++ app/js/app.js
@@ -4,5 +4,6 @@ j2mb.app = angular.module('todoyall',['ngRoute']);
 j2mb.app.config(function($routeProvider) {
   $routeProvider.
       when('/', {templateUrl: 'partials/list.html', controller: 'TodoController'}).
+    when('/details/:index', {templateUrl: 'partials/todoItem.html', controller: 'TodoItemController'}).
       otherwise({redirectTo: '/'});
 });
